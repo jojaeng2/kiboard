19dc639ac6e472834d159f4a6a36280f386fbb34
jojaeng2 <76645095+jojaeng2@users.noreply.github.com>
2021-02-03 14:48:23 +0900

    언어선택기능

--- js/language.js
+++ js/language.js
@@ -1,5 +1,21 @@
 const language = document.querySelector(".language");
 const rightLanguage = document.querySelector(".right-language");
+const select = document.querySelector(".right-language__wrap");
+const languageSelect = select.querySelectorAll("li");
+const languageVar = document.querySelector(".language-var");
+
+
+for (const click of languageSelect) {
+  click.addEventListener("click",function(event){
+  for (i = 0; i < languageSelect.length; i++){
+    languageSelect[i].style.color="black";
+  }
+  click.style.color = "#2ed573";
+  rightLanguage.classList.toggle("language-click");
+  languageVar.innerText = "";
+  languageVar.innerText = click.innerText;
+  });
+}
 
 function seeLanguage(){
   rightLanguage.classList.toggle("language-click");
@@ -8,5 +24,4 @@ function seeLanguage(){
 function init(){
   language.addEventListener("click",seeLanguage);
 }
-
 init();
